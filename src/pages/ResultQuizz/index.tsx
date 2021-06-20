c1131ed4ac8d8e5626832427e62fdd59ac1b7e1d
brendamatias <brendamatias.sobral@gmail.com>
2021-06-20 17:27:34 -0300

    add result quizz screen

---
+++ src/pages/ResultQuizz/index.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+
+import { View, StatusBar } from 'react-native';
+import {
+  Container,
+  Content,
+  ContentTop,
+  Image,
+  Row,
+  Title,
+  SubTitle,
+} from './styles';
+import Button from '../../components/Button';
+import COLORS from '../../constants/theme';
+
+interface IParams {
+  score: number;
+  correctAnswers: number;
+  totalQuestions: number;
+}
+
+const ResultQuizz: React.FC = ({ navigation, route }) => {
+  const { score, correctAnswers, totalQuestions } = route.params;
+
+  const resetQuizz = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Quizz' }],
+    });
+  };
+
+  const completeQuizz = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Quizz' }],
+    });
+
+    navigation.navigate('Dashboard');
+  };
+
+  return (
+    <Container>
+      <StatusBar barStyle="light-content" backgroundColor={COLORS.secondary} />
+      <Content>
+        <ContentTop>
+          <SubTitle fontSize={30} margin="0 0 30px 0">
+            Quiz Result
+          </SubTitle>
+
+          {correctAnswers > totalQuestions / 2 ? (
+            <Image source={require('../../assets/trophy.svg')} />
+          ) : (
+            <Image source={require('../../assets/crying.png')} />
+          )}
+
+          <Title>
+            {correctAnswers > totalQuestions / 2 ? 'Congratulations' : 'Oops'}
+          </Title>
+          <SubTitle fontSize={18} margin="0 0 20px 0">
+            {correctAnswers > totalQuestions / 2
+              ? 'Já pode substituir a Meredith'
+              : 'Ellis Grey ficaria desapontada com você!'}
+          </SubTitle>
+          <Row>
+            <View style={{ alignItems: 'center', marginRight: 60 }}>
+              <SubTitle fontSize={60}>{score}</SubTitle>
+              <SubTitle fontSize={18}>points</SubTitle>
+            </View>
+            <View style={{ alignItems: 'center' }}>
+              <SubTitle fontSize={60}>
+                {correctAnswers}/{totalQuestions}
+              </SubTitle>
+              <SubTitle fontSize={18}>acertos</SubTitle>
+            </View>
+          </Row>
+        </ContentTop>
+
+        <Row>
+          {correctAnswers !== totalQuestions && (
+            <Button
+              style={{ marginRight: 20, backgroundColor: '#24BCAD' }}
+              onPress={() => resetQuizz()}
+              text="Try Again"
+            />
+          )}
+
+          <Button onPress={() => completeQuizz()} text="Complete" />
+        </Row>
+      </Content>
+    </Container>
+  );
+};
+
+export default ResultQuizz;
---
+++ src/pages/ResultQuizz/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native';
+import COLORS from '../../constants/theme';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  padding: 40px 10px;
+  background-color: ${COLORS.secondary};
+  align-self: stretch;
+  align-items: center;
+`;
+
+export const ContentTop = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+`;
+
+export const Image = styled.Image`
+  max-width: 160px;
+  max-height: 160px;
+`;
+
+export const Title = styled.Text`
+  font-size: 40px;
+  letter-spacing: 1px;
+  margin: 40px 0 10px 0;
+  font-weight: bold;
+  color: ${COLORS.white};
+`;
+
+export const SubTitle = styled.Text`
+  font-size: ${props => `${props.fontSize}px`};
+  margin: ${props => (props.margin ? props.margin : '0px')};
+  font-weight: bold;
+  color: ${COLORS.white};
+`;
