c2c959633341f6208d1ef22a23ac34ec0c97bab1
brendamatias <brendamatias.sobral@gmail.com>
2021-06-21 01:32:53 -0300

    add new questions

--- src/data/QuizData.tsx
+++ src/data/QuizData.tsx
@@ -93,57 +93,119 @@ const greysanatomy = [
 
 const modernfamily = [
   {
-    question: 'Preston Burke abandona quem na terceira temporada?',
-    options: [
-      'Izzie Stevens',
-      'Cristina Yang',
-      'Meredith Grey',
-      'Miranda Bailey',
-    ],
-    correct_option: 'Cristina Yang',
+    image: require('../assets/modernfamily1.jpg'),
+    question: 'Em qual país a Lily nasceu?',
+    options: ['Canadá', 'China', 'Brasil', 'Vietnã'],
+    correct_option: 'Vietnã',
   },
   {
-    question: 'Quem é a protagonista da série?',
+    image: require('../assets/modernfamily2.jpg'),
+    question: 'Quais os nomes dos filhos de Claire e Phil?',
     options: [
-      'Izzie Stevens',
-      'Cristina Yang',
-      'Meredith Grey',
-      'Miranda Bailey',
+      'Haley, Alex e Many',
+      'Haley, Alex e Luke',
+      'Joe, Alex e Luke',
+      'Marta, Alex e Luke',
     ],
-    correct_option: 'Meredith Grey',
+    correct_option: 'Haley, Alex e Luke',
   },
   {
-    question: 'A Ellis Grey ganhou quantos Harper Avery?',
-    options: ['Cinco', 'Um', 'Quatro', 'Mais de seis'],
-    correct_option: 'Dois',
+    image: require('../assets/modernfamily3.jpg'),
+    question: 'Qual a nacionalidade da Gloria?',
+    options: ['Colombiana', 'Inglesa', 'Mexicana', 'Cubana'],
+    correct_option: 'Colombiana',
+  },
+  {
+    image: require('../assets/modernfamily1.jpg'),
+    question: 'Qual o ano da primeira temporada de Modern Family?',
+    options: ['2004', '2007', '2009', '2011'],
+    correct_option: '2009',
+  },
+  {
+    image: require('../assets/modernfamily2.jpg'),
+    question: 'Qual personagem fica gravida na 3ª temporada?',
+    options: ['Alex', 'Hailey', 'Claire', 'Gloria'],
+    correct_option: 'Gloria',
+  },
+  {
+    image: require('../assets/modernfamily3.jpg'),
+    question: 'Quantas temporadas tem a série Modern Family?',
+    options: ['08', '09', '10', '11'],
+    correct_option: '11',
+  },
+  {
+    image: require('../assets/modernfamily1.jpg'),
+    question:
+      'Quando Cameron vai dar aula na escola do Luke, qual disciplina ele aplica?',
+    options: ['Teatro', 'Música', 'Química', 'Matemática'],
+    correct_option: 'Música',
+  },
+  {
+    image: require('../assets/modernfamily2.jpg'),
+    question: 'Que instrumento Alex toca?',
+    options: ['Violoncelo', 'Guitarra', 'Banjo', 'Bateria'],
+    correct_option: 'Violoncelo',
+  },
+  {
+    image: require('../assets/modernfamily3.jpg'),
+    question: 'Em que peça teatral Luke ganha o papel principal?',
+    options: [
+      'O barbeiro de Sevilha',
+      'Os miseráveis',
+      'O fantasma da opera',
+      'Os 3 porquinhos',
+    ],
+    correct_option: 'O fantasma da opera',
+  },
+  {
+    image: require('../assets/modernfamily1.jpg'),
+    question: 'Quais nomes Phil e Claire usam na época do dia dos namorados?',
+    options: [
+      'Clive e Juliana',
+      'Tom e Barbara',
+      'Fizbo e Madonna',
+      'Tom e Maggie',
+    ],
+    correct_option: 'Clive e Juliana',
+  },
+  {
+    image: require('../assets/modernfamily2.jpg'),
+    question: 'Qual o nome do vereador contra qual Claire disputa as eleições?',
+    options: ['Jason Tex', 'Duane Bailey', 'Tom Briken', 'Phillip Donson'],
+    correct_option: 'Duane Bailey',
+  },
+  {
+    image: require('../assets/modernfamily3.jpg'),
+    question: 'Por qual motivo Haley é expulsa da universidade?',
+    options: [
+      'Por cola nas provas',
+      'Por namorar um professor',
+      'Por estar bêbada e agredir um policial',
+      'Por matar aulas',
+    ],
+    correct_option: 'Por estar bêbada e agredir um policial',
   },
 ];
 
 const friends = [
   {
-    question: 'Preston Burke abandona quem na terceira temporada?',
-    options: [
-      'Izzie Stevens',
-      'Cristina Yang',
-      'Meredith Grey',
-      'Miranda Bailey',
-    ],
-    correct_option: 'Cristina Yang',
+    image: require('../assets/friends1.jpeg'),
+    question: 'Quem Joey beija na festa de aniversário surpresa da Rachel?',
+    options: ['Phoebe', 'A chefe da Rachel', 'Mônica', 'A mãe da Rachel'],
+    correct_option: 'A mãe da Rachel',
   },
   {
-    question: 'Quem é a protagonista da série?',
-    options: [
-      'Izzie Stevens',
-      'Cristina Yang',
-      'Meredith Grey',
-      'Miranda Bailey',
-    ],
-    correct_option: 'Meredith Grey',
+    image: require('../assets/friends2.jpg'),
+    question: 'Em quem a Monica dá o primeiro beijo de sua vida?',
+    options: ['Chip Mathews', 'Chandler', 'Roy Goobling', 'Ross'],
+    correct_option: 'Ross',
   },
   {
-    question: 'A Ellis Grey ganhou quantos Harper Avery?',
-    options: ['Cinco', 'Um', 'Quatro', 'Mais de seis'],
-    correct_option: 'Dois',
+    image: require('../assets/friends3.jpg'),
+    question:
+      'O que causa uma reação alérgica no Ross, naquele episódio em que Chandler e Joey esquecem o Ben dentro de um ônibus?',
+    options: ['Camarão', 'Amendoim', 'Kiwi', 'Pimenta'],
+    correct_option: 'Kiwi',
   },
 ];
 
